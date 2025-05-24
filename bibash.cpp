739665dc8a54e655ce14681cf7874adb5168c304
Bibash Sharma <sharmabibash77@gmail.com>
2025-05-24 12:13:10 +0545

    1st

---
+++ bibash.cpp
@@ -0,0 +1,16 @@
+/*
+* Classes and Objects
+a) Obects are entities in real world.
+b) Classes is Like a Blueprint of these entities or objects.
+*/
+
+
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    return 0;
+}
